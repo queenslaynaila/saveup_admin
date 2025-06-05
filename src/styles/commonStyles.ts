62e69f27cb91321ba806fd514975e9de51e98899
Queenslay Jema <queenslayjema@gmail.com>
2025-06-05 16:08:44 +0300

    Add responsive font size and error text styles to commonStyles

--- src/styles/commonStyles.ts
+++ src/styles/commonStyles.ts
@@ -1,4 +1,6 @@
 import { css } from "@linaria/atomic";
+import { mq } from "./breakpoints";
+import { TEXT_COLOR, TITLE_COLOR } from "./colors";
 
 export const FlexColumn = css`
   display: flex;
@@ -20,4 +22,44 @@ export const FlexBetweenCenter = css`
 export const FlexCenter = css`
   display: flex;
   align-items: center;
+`;
+
+export const fontSizeStyles = css`
+  font-size:0.875rem;
+
+  ${mq[0]} {  
+    font-size:1.063rem;
+  }
+`;
+
+export const errorText = css`
+  color:#dc233d;
+  font-size: 0.7rem;
+  font-weight: 500;
+  margin-left: 0.4rem;
+  bottom: -0.938rem;
+  position:absolute;
+  ${mq[0]} {
+  bottom: -0.938rem;
+    font-size: 0.8rem;
+  }
+`;
+
+export const TextStyles = css`
+  font-size: 0.813rem;
+  color: ${TEXT_COLOR};
+
+  ${mq[0]} {
+    font-size: 0.938rem;
+  }
+`;
+
+export const TitleStyles = css`
+  font-size: 1.125rem;
+  font-weight: 600;
+  color: ${TITLE_COLOR};
+
+  ${mq[0]} {
+    font-size: 1.25rem;
+  }
 `;
