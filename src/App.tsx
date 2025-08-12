9c81036b6f2320dd58a1ca2934b1aa50d193177b
Queenslay Jema <queenslayjema@gmail.com>
2025-08-12 15:36:00 +0300

    Refactor App component for improved structure and authentication handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { Suspense } from "react";
-import { Route, Switch } from "wouter";
+import { Suspense, type FC } from "react";
+import { Route, Switch, Redirect } from "wouter";
 import Loader from "./components/Loader";
 import { Login } from "./views/Login";
 import Users from "./views/Users";
-import Campaigns from "./views/Campaigns";
 import Groups from "./views/Groups";
 import Pockets from "./views/Pockets";
 
-function App() {
+
+const App: FC = () => {
+  const isAuthenticated = Boolean(localStorage.getItem("USER"));
+
   return (
-    <div>
-      <Suspense fallback={<Loader />}>
-        <Switch>
-          <Route path="/log-in" component={Login} />
-          <Route path="/admin/users" component={Users} />
-          <Route path="/admin/groups" component={Groups} />
-          <Route path="/admin/campaigns" component={Campaigns} />
-          <Route path="/admin/pockets" component={Pockets} />
-        </Switch>
-      </Suspense>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <Switch>
+        <Route path="/log-in" component={Login} />
+        {isAuthenticated && (
+          <>
+            <Route path="/admin/users" component={Users} />
+            <Route path="/admin/groups" component={Groups} />
+            <Route path="/admin/pockets" component={Pockets} />
+          </>
+        )}
+        {!isAuthenticated && <Redirect to="/log-in" />}
+      </Switch>
+    </Suspense>
   );
-}
+};
 
 export default App;
