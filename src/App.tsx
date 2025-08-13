68a2a0ba516dc40a1ed5d7f464fb138cd46915bd
Queenslay Jema <queenslayjema@gmail.com>
2025-08-13 10:58:17 +0300

    Refactor authentication handling in App component with ProtectedRoute for improved route protection

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,36 @@ import { Login } from "./views/Login";
 import Users from "./views/Users";
 import Groups from "./views/Groups";
 import Pockets from "./views/Pockets";
+import { isAuthenticated } from "./utils/IsAuthenticated";
 
+type ProtectedRouteProps = {
+  component: React.FC;
+  path: string;
+};
+
+const ProtectedRoute: FC<ProtectedRouteProps> = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest}>
+      {isAuthenticated() ? <Component /> : <Redirect to="/log-in" />}
+    </Route>
+  );
+};
 
 const App: FC = () => {
-  const isAuthenticated = Boolean(localStorage.getItem("USER"));
-
   return (
     <Suspense fallback={<Loader />}>
       <Switch>
-        <Route path="/log-in" component={Login} />
-        {isAuthenticated && (
-          <>
-            <Route path="/admin/users" component={Users} />
-            <Route path="/admin/groups" component={Groups} />
-            <Route path="/admin/pockets" component={Pockets} />
-          </>
-        )}
-        {!isAuthenticated && <Redirect to="/log-in" />}
+        <Route path="/log-in">
+          {isAuthenticated() ? <Redirect to="/admin/users" /> : <Login />}
+        </Route>
+
+        <ProtectedRoute path="/admin/users" component={Users}  />
+        <ProtectedRoute path="/admin/groups" component={Groups} />
+        <ProtectedRoute path="/admin/pockets" component={Pockets} />
+
+        <Route path="*">
+          {isAuthenticated() ? <Redirect to="/admin/users" /> : <Redirect to="/log-in" />}
+        </Route>
       </Switch>
     </Suspense>
   );
