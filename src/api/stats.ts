339016880693c70bd92e7f266b4eeac0fbac97b3
Queenslay Jema <queenslayjema@gmail.com>
2025-08-14 15:21:53 +0300

    feat: add API functions for authentication, expenses, savings, and withdrawal statistics

---
+++ src/api/stats.ts
@@ -0,0 +1,106 @@
+
+import api from "./config"
+
+export const getAuthenticationStats = async (
+    startDate: string,
+    endDate: string
+): Promise<{
+    total_registrations: number,
+    total_logins: number,
+    total_failed_logins: number,
+    locked_accounts: number
+}> => {
+    try {
+        const params = [];
+
+        if (startDate !== undefined) params.push(`start_date=${startDate}`);
+        if (endDate !== undefined) params.push(`end_date=${endDate}`);
+
+        const queryString = params.length ? `?${params.join("&")}` : "";
+        const response = await api.get(`stats/auth-metrics${queryString}`);
+
+        if (response.status === 200) {
+            return response.data;
+        }
+
+        throw new Error("Failed to fetch auth stats");
+    } catch (error) {
+        return Promise.reject(error);
+    }
+};
+
+export const getExpensesStats = async (
+    startDate: string,
+    endDate: string,
+    agg: 'avg' | 'sum' | 'count' | 'min' | 'max'
+): Promise<{
+    aggregated_expenses: number,
+}> => {
+    try {
+        const params = [];
+
+        if (startDate !== undefined) params.push(`start_date=${startDate}`);
+        if (endDate !== undefined) params.push(`end_date=${endDate}`);
+        if (agg !== undefined) params.push(`agg=${agg}`);
+
+        const queryString = params.length ? `?${params.join("&")}` : "";
+        const response = await api.get(`stats/expenses${queryString}`);
+
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error("Failed to fetch expenses stats");
+    } catch {
+        return Promise.reject(new Error("Failed to fetch expenses stats"));
+    }
+};
+
+export const getSavingStats = async (
+    startDate: string,
+    endDate: string,
+    agg: 'avg' | 'sum' | 'count' | 'min' | 'max'
+): Promise<{
+    aggregated_savings: number,
+}> => {
+    try {
+        const params = [];
+
+        if (startDate !== undefined) params.push(`start_date=${startDate}`);
+        if (endDate !== undefined) params.push(`end_date=${endDate}`);
+        if (agg !== undefined) params.push(`agg=${agg}`);
+
+        const queryString = params.length ? `?${params.join("&")}` : "";
+        const response = await api.get(`stats/savings${queryString}`);
+
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error("Failed to fetch expenses stats");
+    } catch {
+        return Promise.reject(new Error("Failed to fetch expenses stats"));
+    }
+};
+
+export const getWithdrawalStats = async (
+    startDate: string,
+    endDate: string,
+    agg: 'avg' | 'sum' | 'count' | 'min' | 'max'
+): Promise<{ aggregated_withdrawals: number }> => {
+    try {
+        const params = [];
+
+        if (startDate !== undefined) params.push(`start_date=${startDate}`);
+        if (endDate !== undefined) params.push(`end_date=${endDate}`);
+        if (agg !== undefined) params.push(`agg=${agg}`);
+
+        const queryString = params.length ? `?${params.join("&")}` : "";
+        const response = await api.get(`stats/${queryString}`);
+
+        if (response.status === 200) {
+            return response.data;
+        }
+        throw new Error("Failed to fetch expenses stats");
+    } catch {
+        return Promise.reject(new Error("Failed to fetch expenses stats"));
+    }
+};
