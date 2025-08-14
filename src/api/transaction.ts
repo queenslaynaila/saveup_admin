2f35744e53e69541e3c4947748a23d1fafacd13e
Queenslay Jema <queenslayjema@gmail.com>
2025-08-14 09:46:25 +0300

    feat: implement transaction fetching in UserTransactionsTable and update Users view to pass userId

--- src/api/transaction.ts
+++ src/api/transaction.ts
@@ -1,17 +1,25 @@
 import type { Transaction } from "../types/transaction.types"
 import api from "./config"
 
-export const getTransactions = 
-    async (entityId: number): Promise<Transaction[]> => {
+export const getTransactions = async (
+  entityId: number,
+  before?: number,
+  after?: number
+): Promise<Transaction[]> => {
   try {
-    const response = await api.get(`${entityId}/transactions`)
+    const params = [];
+    if (before !== undefined) params.push(`before=${before}`);
+    if (after !== undefined) params.push(`after=${after}`);
+
+    const queryString = params.length ? `?${params.join("&")}` : "";
+    const response = await api.get(`${entityId}/transactions${queryString}`);
 
     if (response.status === 200) {
-      return response.data
+      return response.data;
     }
 
-    throw new Error("Failed to fetch user transactions")
+    throw new Error("Failed to fetch user transactions");
   } catch (error) {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
 }
