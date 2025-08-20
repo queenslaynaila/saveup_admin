056048e044be38a6481566364a5b03ea5a29341c
Queenslay Jema <queenslayjema@gmail.com>
2025-08-20 15:53:08 +0300

    refactor: update Config type to include country_code and created_at fields

--- src/types/configurations.types.ts
+++ src/types/configurations.types.ts
@@ -1,14 +1,14 @@
 export type Config = {
-    id: number;
-    country_name: string;
-    calling_code: number;
-    languages: string[];
-    max_deposit: number;
-    min_deposit: number;
-    max_withdrawal: number;
-    min_withdrawal: number;
-    withdrawal_charges: string;
-    currency: string;
+  id: number;
+  country_code: string;
+  country_name: string;
+  currency: string;
+  calling_code: string;
+  languages: string[];
+  min_deposit: number;
+  max_deposit: number;
+  min_withdrawal: number;
+  max_withdrawal: number;
+  withdrawal_charges: string;
+  created_at: string;
 }
-
-export type ConfigPatchPayload =  & Partial<Omit<Config, "id">>;
